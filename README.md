8d4cf389f82837f9ced7c5f52d203d0fe69f33b9
Yijiao Zhou <yijiao_zhou@berkeley.edu>
2025-08-20 10:20:11 -0700

    Add basic implementation of Newton's method.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # newton-pratice
+
+This is a toy example for computational workshop on 2025-08-20.
---
+++ newton.py
@@ -0,0 +1,21 @@
+import numpy as np
+def optimize(start,fun,max_iter = 10000,tol=1e-7):
+
+    ## calculate derivative
+    def first_derivative(x,fun,h=1e-5):
+        return (fun(x+h)-fun(x-h))/(2*h)
+    def second_derivate(x,fun,h=1e-5):
+        return ( first_derivative(x+h,fun)-first_derivative(x-h,fun))/(2*h)
+
+    x = float(start)
+
+    for i in range(max_iter):
+        f_prime =  first_derivative(x,fun)
+        f_second_prime = second_derivate(x,fun)
+        new_x = x - f_prime/f_second_prime
+        change = abs(x-new_x)
+        if change < tol:
+            break 
+        x = new_x 
+    return x
+        
